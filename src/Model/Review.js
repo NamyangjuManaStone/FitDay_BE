75a5fbd9d008355442c91e61e7c8a01a882c2a21
여찬규 <wry5533@gmail.com>
2022-12-04 18:48:35 +0900

    feat: 코드정리

--- src/Model/Review.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const reviewschema = new mongoose.Schema({
-  value: String,
-});
-
-const Review = mongoose.model("Review", reviewschema);
-
-module.exports = { Review };
--- src/Model/Weight.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const weightschema = new mongoose.Schema({
-  value: Number,
-});
-
-const Weight = mongoose.model("Weight", weightschema);
-
-module.exports = { Weight };
